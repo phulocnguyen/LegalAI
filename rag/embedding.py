c00af66eeee6253cc5080676e7adea8f9b370a6f
Phu Loc Nguyen <phuloc01.nguyen@gmail.com>
2025-01-19 21:10:55 +0700

    done

--- rag/embedding.py
+++ rag/embedding.py
@@ -16,5 +16,5 @@ def get_openai_embedding(model_name="text-embedding-3-small", api_key=None):
 
 if __name__ == "__main__":
     # Bạn có thể truyền API key trực tiếp ở đây nếu cần
-    model = get_openai_embedding(api_key="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")
+    model = get_openai_embedding(api_key="")
     print(model)
