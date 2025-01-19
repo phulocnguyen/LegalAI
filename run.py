09cd2e6f5b7b31851c5d8d13ceb1da21f5549cf5
Phu Loc Nguyen <phuloc01.nguyen@gmail.com>
2025-01-19 21:09:22 +0700

    done

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ from rag.retriever import get_retriever
 
 load_dotenv()
 
-embedding = get_openai_embedding(api_key="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")
+embedding = get_openai_embedding(api_key="")
 retrievers = get_retriever(source="dan_su", embedding=embedding)
-gpt_llm = ChatOpenAI(temperature=0, openai_api_key="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")
+gpt_llm = ChatOpenAI(temperature=0, openai_api_key="")
 basicrag = BasicRAG(retrievers, gpt_llm)
 question = str(input("Tôi có thể giúp gì cho bạn: "))
 ans = basicrag.answer(question=question)
